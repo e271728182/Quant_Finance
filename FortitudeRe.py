7cfabda2f691ceb0b0247173ac37b7780abedc01
Gabriel Patterson <gab.patterson@gmail.com>
2019-01-01 07:55:27 -0500

    Added 3 functions for date time(lastOfMonth, createDatetime from a string

--- FortitudeRe.py
+++ FortitudeRe.py
@@ -327,3 +327,23 @@ def selectByDate(itemList,valDate,dateKey,itemKey):
             __container[__count]=item[itemKey]
             __count=count+1
     return __container
+
+def mkDateTime(dateString,strFormat="%Y-%m-%d"):
+    # Expects "YYYY-MM-DD" string
+    # returns a datetime object
+    eSeconds = time.mktime(time.strptime(dateString,strFormat))
+    return datetime.datetime.fromtimestamp(eSeconds)
+
+def mkLastOfMonth(dtDateTime):
+    dYear = dtDateTime.strftime("%Y")        #get the year
+    dMonth = str(int(dtDateTime.strftime("%m"))%12+1)#get next month, watch rollover
+    dDay = "1"                               #first day of next month
+    nextMonth = mkDateTime("%s-%s-%s"%(dYear,dMonth,dDay))#make a datetime obj for 1st of next month
+    delta = datetime.timedelta(seconds=1)    #create a delta of 1 second
+    return nextMonth - delta                 #subtract from nextMonth and return
+
+def mkFirstOfMonth(dtDateTime):
+    #what is the first day of the current month
+    #format the year and month + 01 for the current datetime, then form it back
+    #into a datetime object
+    return mkDateTime(formatDate(dtDateTime,"%Y-%m-01"))
