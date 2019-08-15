d5e6bde81737085589029fe17a8363d851d1e0ad
Gabriel <gab.patterson@gmail.com>
2019-08-15 16:05:46 -0300

    clean up of calibrate_term_structure

--- bmaOs.py
+++ bmaOs.py
@@ -2,6 +2,8 @@
 import QuantLib as ql
 import pandas as pd
 import numpy as np
+from scipy.optimize import newton, root_scalar
+
 #to correct later on with app
 
 def extract_info_from_curve(valDate,originScenCurve,xSpreads,colNames,spotCurve):
@@ -22,6 +24,40 @@ def extract_info_from_curve(valDate,originScenCurve,xSpreads,colNames,spotCurve)
         rates.append(__scenCurve.zeroRate(yearPassed,ql.Compounded).rate())
     #reutrns a dataframe of date & scenario curve    
     return pd.DataFrame(list(zip(dates, rates)),columns=colNames)
+
+def calibrate_term_structure(baseSpotCurve,listSpreads,startDate,name):
+    dates=list(baseSpotCurve.dates())
+    spreads = [ ql.SimpleQuote(0.0) for n in dates ] # null spreads to begin
+    scenCurve = ql.SpreadedLinearZeroInterpolatedTermStructure(ql.YieldTermStructureHandle(baseSpotCurve),[ql.QuoteHandle(q) for q in spreads],dates)
+    
+   
+    colname=name
+    for run in range(1,3):
+        for t in range(1,100):
+            if t>35:
+
+            #target=spreads[t].value()
+                target=listSpreads[-1]
+            else:
+                target=listSpreads[t]    
+            #print(target)
+            timePeriod=dates[t-1]
+            myGuess=target*1.05
+            newton(forward_match,myGuess,args=(target,startDate,timePeriod,spreads,scenCurve,baseSpotCurve,t))
+    return scenCurve        
+
+def extract_info(curve,dates,valDate,name='spot_rate'):
+    rates=[]
+    for t in range(0,len(dates)):
+        #years=years+1
+        date=dates[t]
+        #nb years between valuation date and future date
+        yearPassed=ql.ActualActual().yearFraction(valDate, date)
+
+        rates.append(curve.zeroRate(yearPassed,ql.Compounded).rate())
+    #reutrns a dataframe of date & scenario curve    
+    return pd.DataFrame(list(zip(dates, rates)),columns=['date',name])
+
 
 def spot_rate_match(guess,ScenCurve,baseCurve,todayDate,spotDate,spreads,target,indx):
     
