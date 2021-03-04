c7d4abc4dd0d15a479d957142e208d7c721cce34
Gabriel <gab.patterson@gmail.com>
2021-03-03 20:10:08 -0500

    acturial updates

--- quantFin.py
+++ quantFin.py
@@ -5,8 +5,8 @@ import numpy as np
 from scipy.optimize import newton, root_scalar
 from dataclasses import dataclass,field
 from typing import List
-
-
+from collections import defaultdict
+import numpy as np
 #to correct later on with app
 #name convention for variables,functions and classes
 #Since functions and classes depend largely on QuantLib, the same naming convention has been applied
@@ -42,6 +42,159 @@ class ActuarialModel:
     outputs:List=field(default_factory=lambda: [])
     results:List=field(default_factory=lambda: [])
 
+        
+@dataclass
+class ActuarialBucket:
+    id:int=0
+    policies:List=field(default_factory=lambda: [])
+    assumption_set_key:int=0
+    valuationDate:ql.Date=ql.Date(27, 10, 1929)
+    valuation_period:int=1 #1=annual 2=semi-annual 3=quaterly 12=monthly        
+    actuarial_decrements:defaultdict=field(default_factory=lambda: {})
+    keep_data:bool=False
+    ####methods###
+    
+    def calculate_Sx(self,policy,wareHouse):
+        """
+        returns a numpy array with a beginning of year structure
+        """
+        ass_set=wareHouse.assump_set_dept[self.assumption_set_key]
+        mortality=wareHouse.mort_dept[ass_set.mortality_id]
+        
+        key=key=str(ass_set.mortality_id)+'-'+str(policy.age)+'-sx'
+        if key in self.actuarial_decrements.keys():
+            sx=self.actuarial_decrements[key]
+        
+        else:
+            sx=self.gomp_makeham_surv(np.arange(policy.age-1,115)\
+                                              ,mortality.alpha_makeham,mortality.beta_makeham,mortality.gamma_makeham)
+                         
+            sx=sx/sx[0]
+            if self.keep_data==True:
+                self.actuarial_decrements[key]=sx
+        return sx
+    
+    def calculate_life_exp(self,policy,wareHouse):
+        return sum(self.calculate_Sx(policy,wareHouse))-1
+        
+    
+    def gomp_makeham_surv(self,age,alpha,beta,gamma):
+        """
+        Survival function 
+
+        """   
+        return np.exp(-alpha*age-gamma/beta*(np.exp(beta*age))) 
+    
+    def calculate_dx(self,policy,wareHouse):
+        
+        ass_set=wareHouse.assump_set_dept[self.assumption_set_key]
+        mortality=wareHouse.mort_dept[ass_set.mortality_id]
+        
+        key=key=str(ass_set.mortality_id)+'-'+str(policy.age)+'-dx'
+        
+        if key in self.actuarial_decrements.keys():
+            dx=self.actuarial_decrements[key]
+        
+        else:
+            sx=self.calculate_Sx(policy,wareHouse)
+            dx=-np.ediff1d(sx)
+            if self.keep_data==True:
+                self.actuarial_decrements[key]=dx
+        return dx
+     
+    def calculate_qx(self,policy,wareHouse):
+        
+        ass_set=wareHouse.assump_set_dept[self.assumption_set_key]
+        mortality=wareHouse.mort_dept[ass_set.mortality_id]
+        
+        key=str(ass_set.mortality_id)+'-'+str(policy.age)+'-qx'
+        
+        if key in self.actuarial_decrements.keys():
+            qx=self.actuarial_decrements[key]
+        else:
+            sx=self.calculate_Sx(policy,wareHouse)
+            dx=self.calculate_dx(policy,wareHouse)
+            qx=dx
+            qx=dx/sx[:-1]
+            if self.keep_data==True:
+                self.actuarial_decrements[key]=qx
+        
+        return qx   
+
+    def calculate_sx_lx_with_lapse(qx,lapse_x):
+    
+        lxWithLapse=np.zeros(qx.size)
+        sxWithLapse=np.zeros(qx.size)
+    
+        sxWithLapse[0]=1
+        lxWithLapse[0]=qx[0]
+        for index in range(1,qx.size):
+        
+            sxWithLapse[index]=sxWithLapse[index-1]*(1-qx[index-1])*(1-lapse_x[index-1])
+            lxWithLapse[index]=sxWithLapse[index]*qx[index]
+        return sxWithLapse,lxWithLapse 
+    
+    
+
+
+@dataclass
+class AssumptionWareHouse:
+    mort_dept:defaultdict=field(default_factory=lambda: {})
+    lapse_dept:defaultdict=field(default_factory=lambda: {})
+    assump_set_dept:defaultdict=field(default_factory=lambda: {})         
+        
+        
+@dataclass
+class Cell:
+    wareHouse:AssumptionWareHouse=AssumptionWareHouse()
+    mitos:List=field(default_factory=lambda: []) 
+
+@dataclass
+class AssumptionSet:
+    """
+    
+    """
+    id:int=666
+    mortality_id:int=0
+    lapse_id:str=0
+    desc:str='' 
+    name:str=''    
+    
+
+        
+@dataclass
+class MortalityAssumption:
+    id:int=0
+    table_start_age=0
+    soa_table_name:str=''
+    gender:bool=True
+    select_period:int=0
+    ultimate_period:int=0
+    makeham_alpha=0
+    makeham_beta=0
+    makeham_gamma=0
+
+@dataclass
+class LapseAssumption:
+    id:int=0
+    lapse_scheduleList:List=field(default_factory=lambda: [])
+    
+        
+@dataclass
+class policyHolder:
+    age:float=125.0
+    is_male:bool=True
+    face_amount=0
+    act_assumpset_id:int=999999
+    discount_asump_id:int=99999
+    premiums_schedule:List=field(default_factory=lambda: [])
+    claims_schedule:List=field(default_factory=lambda: [])
+    inforce_time_left:float=115
+    inception_date:ql.Date=ql.Date(27, 10, 1929)
+    sx:float=0
+    birthday:ql.Date=ql.Date(27, 10, 1929)
+  
+    
 @dataclass
 class Results:
     """
@@ -50,12 +203,15 @@ class Results:
     a result has a unique key and stores the key of its parent model and keep it as a foreign key
     """
     id:int=333333
-    model_Key:int=112233
-    output_Key:int=666
+    model_key:int=112233
+    output_key:int=666
     key:str='aaa??bbb??cc'
     metric:str='npv'
     value:float=0
+
     ingredients:List=field(default_factory=lambda: [])
+        
+
         
 @dataclass
 class DateTimeStruct:
@@ -67,7 +223,7 @@ class DateTimeStruct:
     """
     effectiveDate:ql.Date = ql.Date(30, 9, 2019)
     terminationDate = ql.Date(30, 9, 2118)
-    tenor = ql.Period(ql.Annual)
+    tenor = ql.Annual #Annual-->1 Monthly-->12
     calendar = ql.UnitedStates()
     businessConvention = ql.Following
     terminationBusinessConvention = ql.Following
@@ -87,6 +243,29 @@ class IntRatesStruct:
     compoundingFrequency = ql.Annual
     dayCount=ql.ActualActual()
     isFlat=True
+
+def create_cashflows(self,startTime,freq,values):
+    """
+    DESC:
+    creates cash flows based on a numpy array of values a starting date and a frequency 
+    which increment the startTime in fixed steps
+    INPUTS:
+    startTime:Date=the starting valuation date ql.date
+    freq:str= an string defining the metric of time 1->annual  12->monthly
+    values:numpy array
+        
+     OUTPUT:
+     a list of SimpleCashFlow objects
+     """
+    cashflows=[]
+    for value in values:
+        elapsedTime=np.where(values == value)[0][0] +1 #first index is always 0
+        per=ql.Period(int(12*freq*elapsedTime),ql.Months) #must be multiplied by 12 because of freq basis
+        futureTime=ql.Date.endOfMonth(startTime+per)
+        cf=ql.SimpleCashFlow(value,futureTime)
+        cashflows.append(cf)
+    return cashflows    
+    
     
 def create_schedule(datetimeStruct):
     """
@@ -117,19 +296,25 @@ def create_schedule(datetimeStruct):
     return schedule
 
 
-def create_spotCurve(spotValues,schedule,datetimeStruct,intRateStruct):
+def create_spotCurve(spotValues,startDate,datetimeStruct,intRateStruct):
     """
     DESC:
     creates a spot curve
     INPUTS:
     spotValues:list=list of spot rates  based on annual time for varying duration
+    startDate=the time point where cash flows are discounted to
     schedule:Quantlib.Schedule=a schedule object for managing time and days convention
     dateTimeStruct:DatetimeStruct= to pass on inputs related to time
     intRateStruct:IntRatesStruct=to pass on inputs related to compounding methodology
+    
     OUTPUT
     a Quantlib.ZeroCurve
     """
-    
+    spotDates=[]
+    nbOfSpots=len(spotValues)
+    for i in range(0,nbOfSpots):
+        mydate=ql.Date.endOfMonth(startDate+ql.Period(12*datetimeStruct.tenor*i,ql.Months))
+        spotDates.append(mydate)
     
     __calendar = datetimeStruct.calendar
     __interpolation = intRateStruct.interpolation
@@ -140,13 +325,15 @@ def create_spotCurve(spotValues,schedule,datetimeStruct,intRateStruct):
     spotCurveHandle = ql.YieldTermStructureHandle(spotCurve)
     return spotCurve
 
+
+
 #create curve
-def create_discount_curve(actModelStruct,rates,intRateStruct):
+def create_discount_curve(valDate,rates,intRateStruct):
     """
     DESC:
     create a discount curve for cash flows
     INPUTS:
-    actModelStruct:ActuarialOutput Data structure 
+    valdDate=Quantlib date object 
     rates: either a float if its a flat curve discount or a QuantLib.ZeroCurve object if not
     intRateStruct: interest rate structure to get day count
     OUTPUT
@@ -154,14 +341,20 @@ def create_discount_curve(actModelStruct,rates,intRateStruct):
     """
     
     if intRateStruct.isFlat==True:
-        discount_curve=ql.YieldTermStructureHandle(ql.FlatForward(actModelStruct.valuationDate, rates, intRateStruct.dayCount))
+        discount_curve=ql.YieldTermStructureHandle(ql.FlatForward(valDate, rates, intRateStruct.dayCount))
     else:
         if type(rates)!=ql.QuantLib.YieldTermStructureHandle:
             pass
         discount_curve=ql.YieldTermStructureHandle(rates)
     return discount_curve
 
-
+def gomp_makeham_surv(age,alpha,beta,gamma):
+    """
+    Survival function on the gompertz makeham model of mortality 
+    
+    """
+    
+    return np.exp(-alpha*age-gamma/beta*(np.exp(beta*age)))
 
 def period_multiplier(freq):
     """
@@ -169,6 +362,8 @@ def period_multiplier(freq):
     transform a string input representing a time period such as monthly quaterly annually into an integer
     
     """
+    if type(freq)==int:
+        return freq
     if freq.lower()=='q':
         return 3
     elif freq.lower()=='m':
